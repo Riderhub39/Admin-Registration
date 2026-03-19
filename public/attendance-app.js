45d3c3d747f77284c8f9fd3baa16282985d35712
KHACHUN <limkc_wm22@student.tarc.edu.my>
2026-03-19 12:27:13 +0800

    3

--- public/attendance-app.js
+++ public/attendance-app.js
@@ -787,7 +787,7 @@ window.openMonthlyReportModal = () => {
 
     userList.forEach(u => {
         if (u.status !== 'disabled') {
-           staffSelect.innerHTML += `<option value="${u.authUid || u.docId}">[${u.empCode || 'N/A'}] ${u.name}</option>`;
+           staffSelect.innerHTML += `<option value="${u.authUid || u.docId}">[${u.docId || 'N/A'}] ${u.name}</option>`;
         }
     });
 
